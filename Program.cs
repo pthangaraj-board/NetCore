27064d3a090eea344b59f5fa88493fc49a98bbfb
pthangaraj-board <65935374+pthangaraj-board@users.noreply.github.com>
2020-05-28 07:51:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strList = new List<string>();
+
+            strList.Add("Test");
+            strList.Add("of");
+            strList.Add("NetCore");
+
+            foreach(var str in strList ) {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
